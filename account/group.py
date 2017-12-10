87bcdaa8686f29ad94e0db92155fb01557c8ec46
StarBfly <margot.blackwood@gmail.com>
2017-12-10 17:18:15 +0300

    Fixes according code review was applied.

--- account/group.py
+++ account/group.py
@@ -1,12 +1,8 @@
 class Group:
     def __init__(self, group_data, groupmates):
-        if group_data is None:
-            raise ValueError("Group data is undefined.")
         self._name = group_data.get("name")
         self._groupmates = groupmates
 
     def send_to(self, message):
-        if not message:
-            raise ValueError("Message is undefined.")
         for groupmate in self._groupmates:
             groupmate.send_to(message)
--- endpoints/endpoint.py
+++ endpoints/endpoint.py
@@ -3,8 +3,6 @@ from abc import ABCMeta, abstractmethod
 
 class Endpoint(metaclass=ABCMeta):
     def __init__(self, host, port):
-        if host is None or port is None:
-            raise ValueError("Host or Port are undefined.")
         self._host = host
         self._port = port
 
